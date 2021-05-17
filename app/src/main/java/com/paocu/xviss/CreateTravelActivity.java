37863c300e56931dc05c5efd2c7b586cee927763
sebastian nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2021-05-17 12:44:41 -0500

    adding view select rol

---
+++ app/src/main/java/com/paocu/xviss/CreateTravelActivity.java
@@ -0,0 +1,15 @@
+package com.paocu.xviss;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateTravelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_travel);
+    }
+
+}
